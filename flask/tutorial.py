b35e3ddde0d571aaf83d741994768b50358e65e8
wangchunlei <wangchunlei@github.com>
2014-11-28 16:30:30 +0800

    add build script

--- flask/tutorial.py
+++ flask/tutorial.py
@@ -1,20 +1,44 @@
-from flask import Flask, Response, request
+#!/usr/bin/env python
+from flask import Flask, Response, request, json
 from subprocess import check_output
+import xml.etree.ElementTree as ET
 
 app = Flask(__name__)
 
 
-@app.route('/build')
+@app.route('/', methods=['GET'])
 def api_root():
-    args = request.args
-    cmd = [args.get('cmd'), args.get('file'), '-o', args.get('out')]
-    print cmd
-    try:
-        out = check_output(cmd)
-        return out + "</br>Build Success!"
-    except Exception, e:
-        return e
+    return 'Domas linux build server!'
 
+
+@app.route('/build', methods=['POST'])
+def api_post():
+    data = request.get_json(force=True)
+    company = data['company']
+    product = data['product']
+    copyright = data['copyright']
+    version = data['version']
+    fileversion = data['fileversion']
+    buildversion = data['buildversion']
+    tree = ET.parse('./config.xml')
+    root = tree.getroot()
+    for ele in root:
+        if ele.tag == 'Company':
+            ele.text = company
+        elif ele.tag=='Product':
+        	ele.text=product
+        elif ele.tag=='CopyRight':
+        	ele.text=copyright
+        elif ele.tag=='Version':
+        	ele.text=version
+        elif ele.tag=='FileVersion':
+        	ele.text=fileversion
+        elif ele.tag=='BuildVersion':
+        	ele.text=buildversion
+
+    tree.write('./config.xml', encoding="UTF-8")
+    out = check_output(['./compileproject'])
+    return out+'Build Success!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
