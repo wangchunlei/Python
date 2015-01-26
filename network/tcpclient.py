08b41eb154b337cee4d45295972b307b94a03662
wangchunlei <wcl_orange@163.com>
2015-01-26 22:16:22 +0800

    123
    123

--- network/tcpclient.py
+++ network/tcpclient.py
@@ -1,16 +1,16 @@
 import socket
 
-target_host = "127.0.0.1"
-target_port = 9999
+target_host = "www.baidu.com"
+target_port = 80
 
-# create a socket object 
+# create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # connect the client
-client.connect((target_host, target_port))
+client.connect((target_host,target_port))
 
 # send some data
-client.send("ABCDEFG")
+client.send("GET / HTTP/1.1\r\nHost: baidu.com\r\n\r\n")
 
 # receive some data
 response = client.recv(4096)
