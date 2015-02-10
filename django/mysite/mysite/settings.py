47ef7d465c9320b244c9a9cce90f11aa56a60a04
pubby <wcl_orange@163.com>
2015-02-10 14:29:27 +0800

    commit

--- django/mysite/mysite/settings.py
+++ django/mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'gallery',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -82,3 +83,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+ROOT_URL = '/gallery/'
+MEDIA_URL = ROOT_URL + 'media/'
